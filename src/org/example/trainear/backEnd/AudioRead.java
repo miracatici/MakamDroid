3225aa69d6cbbd7a793b950bf01abb9654add5c7
Bilge Mirac Atici <miracatici@gmail.com>
2016-09-04 01:17:04 +0300

    ds

--- src/org/example/trainear/backEnd/AudioRead.java
+++ src/org/example/trainear/backEnd/AudioRead.java
@@ -1,10 +1,5 @@
 package org.example.trainear.backEnd;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import org.apache.commons.io.IOUtils;
 import org.example.trainear.utilities.AudioUtilities;
 
